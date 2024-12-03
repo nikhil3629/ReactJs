af239e1523ccaad6ff10cc0415543c06d3241daf
Satrasala Nikhil Kumar <126808578+nikhil3629@users.noreply.github.com>
2024-12-03 12:18:29 +0530

    Add files via upload

---
+++ React Class/tr.js
@@ -0,0 +1,62 @@
+var tr = [
+    
+        
+    {
+    "Name": "Mahatma Gandhi",
+    "Age": 78,
+    "Gender": "Male",
+    "Role": "Leader of the non-violent independence movement",
+    "Contribution": "Led India to independence through peaceful protests like the Salt March."
+     },
+     {
+    "Name":"Bhagat Singh",
+    "Age": 23,
+    "Gender": "Male",
+    "Role": "Revolutionary freedom fighter",
+    "Contribution": "Participated in significant acts against British rule, including bombing the Central Legislative Assembly."
+    },
+    {
+    "Name":"Rani Lakshmibai",
+    "Age": 29,
+    "Gender": "Female",
+    "Role": "Queen of Jhansi",
+    "Contribution": "Fought against British forces in the 1857 revolt."
+    },
+    {
+    "Name":"Subhas Chandra Bose",
+    "Age": 48,
+    "Gender": "Male",
+    "Role": "Founder of the Indian National Army",
+    "Contribution": "Sought India’s freedom through military means during World War II."
+    },
+    {
+    "Name":"Sardar Vallabhbhai Patel",
+    "Age": 75,
+    "Gender": "Male",
+    "Role": "First Deputy Prime Minister of India",
+    "Contribution": "Key leader in the unification of Indian states post-independence."
+    },
+    {
+    "Name":"Jawaharlal Nehru",
+    "Age": 74,
+    "Gender": "Male",
+    "Role": "First Prime Minister of India",
+    "Contribution": "Played a major role in India's independence and establishing a democratic government."
+    },
+    {
+    "Name":"Sarojini Naidu",
+    "Age": 70,
+    "Gender": "Female",
+    "Role": "Poet and political leader",
+    "Contribution": "Participated in the Civil Disobedience Movement and became the first woman to become President of the Indian National Congress."
+    },
+    {
+    "Name":"Chandrasekhar Azad",
+    "Age": 24,
+    "Gender": "Male",
+    "Role": "Revolutionary leader",
+    "Contribution": "Known for his valiant fight against British forces, involved in many acts of rebellion."
+    }
+    
+        
+    ]
